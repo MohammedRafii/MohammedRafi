86354cd43d913e1ef7aa1a96072b013976ea9370
MohammedRafii <121567736+MohammedRafii@users.noreply.github.com>
2023-08-08 00:36:04 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import { HiArrowNarrowRight } from "react-icons/hi";
 import { Link } from "react-scroll";
 const Home = () => {
   return (
-    <div className="bg-[#0b0e0a] w-full min-h-screen" id="home">
+    <div className="bg-[#0b0e0a] w-screen min-h-screen" id="home">
       <div className=" max-w-[950px] mx-auto flex flex-col relative top-[30vh] md:top-[25vh]  sm:top-[20vh]  justify-center  px-10">
         <p className="relative text-[#81effd] z-2">
           Hi, I am
