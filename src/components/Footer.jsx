ef4cb922fdb9b080d98f2c20e270ab186f69f34a
MohammedRafii <121567736+MohammedRafii@users.noreply.github.com>
2023-08-17 00:11:31 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Resume from '../assets/Resume.pdf';
+import Resume from '../assets/myResume.pdf';
 import { FaBars, FaTimes, FaGithub, FaLinkedinIn } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
